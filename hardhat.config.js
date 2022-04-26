53aa6761a5fffab2c24bac3a014f41a84910f7df
Mikkel Malmberg <mikkel@brnbw.com>
2022-04-26 22:01:50 +0200

    🔨

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,26 @@ const dotenv = require("dotenv");
 dotenv.config();
 dotenv.config({ path: `.env.${process.env.NODE_ENV}`, overide: true });
 
+const networks = process.env.CI
+  ? { hardhat: {} }
+  : {
+      mainnet: {
+        url: process.env.MAINNET_ENDPOINT,
+        accounts: [process.env.PRIVATE_KEY],
+      },
+      rinkeby: {
+        url: process.env.RINKEBY_ENDPOINT,
+        accounts: [process.env.PRIVATE_KEY],
+      },
+    };
+
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
-// require("hardhat-gas-reporter");
+
+if (process.env.REPORT_GAS) {
+  require("hardhat-gas-reporter");
+}
 
 module.exports = {
   paths: {
@@ -27,16 +43,7 @@ module.exports = {
       },
     },
   },
-  networks: {
-    mainnet: {
-      url: process.env.MAINNET_ENDPOINT,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_ENDPOINT,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
+  networks,
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
